d214ec6410c0dd969ec79df68027bf6a0093daae
PETERS <cp0973791@dt050565.otc.edu>
2025-12-03 09:55:21 -0600

    purchase fullname

--- partials/checkout_success.php
+++ partials/checkout_success.php
@@ -1,6 +1,6 @@
 <h2>Thank you for your purchase!</h2>
 
-<p>Order for <strong><?php echo($full_name)?></strong> completed.</p>
+<p>Order for <strong><?php htmlspecialchars($_SESSION['full_name']) ?></strong> completed.</p>
 
 <p><a href="?view=list" class="btn btn-primary">Back to Records</a></p>
 
